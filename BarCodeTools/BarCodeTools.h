a01fd5e9925f9c87fe43ed7dcf05efa8c3e9a8b3
Jack Wright <jackrwright1@gmail.com>
2019-10-31 07:14:10 -0400

    Initial Commit

---
+++ BarCodeTools/BarCodeTools.h
@@ -0,0 +1,19 @@
+//
+//  BarCodeTools.h
+//  BarCodeTools
+//
+//  Created by Jack Wright on 10/31/19.
+//  Copyright © 2019 Jack Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BarCodeTools.
+FOUNDATION_EXPORT double BarCodeToolsVersionNumber;
+
+//! Project version string for BarCodeTools.
+FOUNDATION_EXPORT const unsigned char BarCodeToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BarCodeTools/PublicHeader.h>
+
+
